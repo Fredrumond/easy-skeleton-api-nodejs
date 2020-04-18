dddb4077704adbed161748f6dc58f282e5807cfd
Frederico <fredrumond@gmail.com>
2020-04-18 18:30:59 -0300

    - Arquivos base para rodar API
     Changes to be committed:
    	new file:   .env.example
    	new file:   .gitignore
    	new file:   nodemon.json
    	new file:   package.json
    	new file:   src/app.js
    	new file:   src/config/database.js
    	new file:   src/database/index.js
    	new file:   src/routes.js
    	new file:   src/server.js
    	new file:   yarn.lock

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: process.env.DB_SERVER,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  logging: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
