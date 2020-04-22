e82f773a34c21e960c17f5fb5d070a10da993272
Frederico <fredrumond@gmail.com>
2020-04-21 22:22:39 -0300

    Removendo arquivos e configuracoes antigas

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: [
+    '**/src/**/*.js',
+    '!**/src/database/migrations/**',
+    '!**/src/database/seeders/**',
+    '!**/src/app.js',
+    '!**/src/routes.js',
+    '!**/src/server.js'
+  ]
 }
--- src/app/controllers/estudo.test.js
+++
@@ -1,5 +0,0 @@
-describe('ESTUDO', () => {
-  it('estou estudando', async () => {
-    expect(201).toEqual(201)
-  })
-})
