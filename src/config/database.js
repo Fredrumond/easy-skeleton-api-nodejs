c84aaa2e64ffc189f67720e05d268f63b0cd8c39
Frederico <fredrumond@gmail.com>
2020-05-09 14:25:28 -0300

    Ajuste sequelizerc Ajuste config/database.js Ajuste .env.example

--- src/config/database.js
+++ src/config/database.js
@@ -1,41 +1,18 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
 module.exports = {
-  development: {
-    dialect: 'mysql',
-    host: process.env.DB_DEV_SERVER,
-    username: process.env.DB_DEV_USER,
-    password: process.env.DB_DEV_PASS,
-    database: process.env.DB_DEV_NAME,
-    logging: true,
-    define: {
-      timestamp: true,
-      underscored: true,
-      underscoredAll: true
-    }
-  },
-  test: {
-    dialect: 'mysql',
-    host: process.env.DB_TEST_SERVER,
-    username: process.env.DB_TEST_USER,
-    password: process.env.DB_TEST_PASS,
-    database: process.env.DB_TEST_NAME,
-    logging: false,
-    define: {
-      timestamp: true,
-      underscored: true,
-      underscoredAll: true
-    }
-  },
-  production: {
-    dialect: 'mysql',
-    host: process.env.DB_SERVER,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    logging: false,
-    define: {
-      timestamp: true,
-      underscored: true,
-      underscoredAll: true
-    }
+  host: process.env.DB_SERVER,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || 'mysql',
+  storage: './tests/database.sqlite',
+  logging: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
   }
 }
