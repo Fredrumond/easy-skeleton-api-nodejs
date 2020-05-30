919d45df677eef6302f47e9c2fa132f6950ca042
Frederico <fredrumond@gmail.com>
2020-05-29 22:41:46 -0300

    Criando httpResponse 200,201 e 404

--- src/app/controllers/helpers/http-response.js
+++ src/app/controllers/helpers/http-response.js
@@ -1,7 +1,34 @@
 module.exports = class HttpResponse {
+  static ok (body) {
+    return {
+      statusCode: 200,
+      body: {
+        body
+      }
+    }
+  }
+
+  static created (body) {
+    return {
+      statusCode: 201,
+      body: {
+        body
+      }
+    }
+  }
+
   static badRequest (error) {
     return {
       statusCode: 400,
+      body: {
+        error: error.message
+      }
+    }
+  }
+
+  static notFound (error) {
+    return {
+      statusCode: 404,
       body: {
         error: error.message
       }
