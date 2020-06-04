b2b8f2f3e281785df8694bafa586850dd8c18941
Frederico <fredrumond@gmail.com>
2020-06-04 20:35:34 -0300

    Refatorando password validator para uma classe

--- tests/unit/password-validator.spec.js
+++ tests/unit/password-validator.spec.js
@@ -1,18 +1,21 @@
-const { validPassword } = require('../../src/utils/ValidPassword')
+const PasswordValidator = require('../../src/utils/helpers/password-validator')
 
-describe('utils/validPassword', () => {
+describe('utils/helpers/password-validator', () => {
   it('should return FALSE if the password provided is less than 6 characters', () => {
-    const invalidPass = validPassword('123')
+    const sut = new PasswordValidator()
+    const invalidPass = sut.isValid('123')
     expect(invalidPass).toEqual(false)
   })
 
   it('should return TRUE if the password provided is equal than 6 characters', () => {
-    const validPass = validPassword('123456')
+    const sut = new PasswordValidator()
+    const validPass = sut.isValid('123456')
     expect(validPass).toEqual(true)
   })
 
   it('should return TRUE if the password provided is further than 6 characters', () => {
-    const validPass = validPassword('1234567')
+    const sut = new PasswordValidator()
+    const validPass = sut.isValid('1234567')
     expect(validPass).toEqual(true)
   })
 })
