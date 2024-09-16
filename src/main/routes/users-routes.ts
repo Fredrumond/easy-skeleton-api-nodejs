a0dab2cf1130d2f8fe7092d816c39ac0825a0feb
fredericodrumond <frederico.drumond@playkids.com>
2024-09-16 18:36:41 -0300

    rewrite user update test api

--- src/main/routes/users-routes.ts
+++ src/main/routes/users-routes.ts
@@ -2,8 +2,10 @@ import { UserIndexRouterComposer} from '@src/main/composers/user-index-router-co
 import { ExpressRouterAdapter } from '@src/main/adapters/express-router-adapter'
 import {UserCreateRouterComposer} from "@src/main/composers/user-create-router-composer";
 import {UserShowRouterComposer} from "@src/main/composers/user-show-router-composer";
+import {UserUpdateRouterComposer} from "@src/main/composers/user-update-router-composer";
 export default (router: any): void => {
     router.get('/users',ExpressRouterAdapter.adapt(UserIndexRouterComposer.compose()));
     router.post('/users',ExpressRouterAdapter.adapt(UserCreateRouterComposer.compose()))
     router.get('/users/:id', ExpressRouterAdapter.adapt(UserShowRouterComposer.compose()))
+    router.put('/users/:id',ExpressRouterAdapter.adapt(UserUpdateRouterComposer.compose()))
 }
--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -128,3 +128,67 @@ describe('User Show', () => {
         expect(body.message).toEqual('User not found!')
     })
 })
+
+describe('User Update', () => {
+    it('Should return 404, if user not registred', async () => {
+        const {body, status} = await supertest(app)
+            .put('/api/users/100')
+        expect(status).toEqual(404)
+        expect(body.message).toEqual('User not found!')
+    })
+
+    it('Should return 400 if the password provided is less than 6 characters', async () => {
+        const fakeUserData = await generateFakeUser();
+
+        const {body, status} = await supertest(app)
+            .put(`/api/users/${fakeUserData.id}`).send({
+                password: '12345'
+            })
+
+        expect(status).toEqual(400)
+        expect(body).toEqual({ error: 'Invalid param: Password must contain 6 characters or more' })
+    })
+
+    it('Should return 400 if no confPassword is provided', async () => {
+        const fakeUserData = await generateFakeUser();
+
+        const {body, status} = await supertest(app)
+            .put(`/api/users/${fakeUserData.id}`).send({
+                password: '123456'
+            })
+        expect(status).toEqual(400)
+        expect(body).toEqual({ error: 'Missing param: confPassword' })
+    })
+
+    it('Should return 400 if password is diff confPassword', async () => {
+        const fakeUserData = await generateFakeUser();
+
+        const {body, status} = await supertest(app)
+            .put(`/api/users/${fakeUserData.id}`).send({
+                password: '123456',
+                confPassword: '123456789'
+            })
+        expect(status).toEqual(400)
+        expect(body).toEqual({ error: 'Missing param: Passwords do not match' })
+    })
+
+    it('Should return 400 if email provided is invalid', async () => {
+        const fakeUserData = await generateFakeUser();
+
+        const {body, status} = await supertest(app)
+            .put(`/api/users/${fakeUserData.id}`).send({
+                email: 'invalid_email'
+            })
+        expect(status).toEqual(400)
+        expect(body).toEqual({ error: 'Invalid param: email' })
+    })
+
+    it('Should return 200, if the user informed to update exists', async () => {
+        const fakeUserData = await generateFakeUser();
+
+        const {body, status} = await supertest(app)
+            .put(`/api/users/${fakeUserData.id}`)
+        expect(status).toEqual(200)
+        expect(body.message).toEqual('User edited successfully!')
+    })
+})
